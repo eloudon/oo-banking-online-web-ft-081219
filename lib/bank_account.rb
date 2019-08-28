425439c0a3905c452a29e54608ad822ef3fced8d
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 11:18:42 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,9 +2,9 @@ class BankAccount
 
 attr_accessor :bank_account, :name
 
-def initialize(bank_account)
-  @bank_account = bank_account
-  @name = name
+def initialize(bank_account,name)
+  @bank_account= bank_account
+  @name= name
 
 end
 
