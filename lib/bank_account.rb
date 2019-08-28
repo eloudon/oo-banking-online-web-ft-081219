2b7e0115a5b074469da7c11d53ac14bb6f3ec6ca
Eric Loudon <loudon.eric@gmail.com>
2019-08-28 11:23:24 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
 
-attr_accessor :bank_account, :name, :balance
+attr_accessor :bank_account, :name, :balance, :status
 
-def initialize(bank_account, name= "Avi", balance = 1000)
+def initialize(bank_account, name= "Avi", balance = 1000, status = 'open')
   @bank_account= bank_account
   @name= name
   @balance= balance
+  @status= status
 
 end
 
